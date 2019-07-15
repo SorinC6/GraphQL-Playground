f5c2fd558e95e6d6ababae6bf1aa1618dff6cc57
Sorin Chis <kish.sorin@yahoo.com>
2019-07-15 15:44:59 +0300

    added Apollo setup for React App

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+// components
+import BookList from "./Components/BookList";
+
+//apollo client setup
+const client = new ApolloClient({
+  uri: "http://localhost:4000/test"
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div id="main">
+        <h1>Woop Code Reading List</h1>
+        <BookList />
+      </div>
+    </ApolloProvider>
   );
 }
 
---
+++ client/src/Components/BookList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const BookList = () => {
+  return (
+    <div>
+      <ul id="book-list">
+        <li>Book Name here</li>
+      </ul>
+    </div>
+  );
+};
+
+export default BookList;
